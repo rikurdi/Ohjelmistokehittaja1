bbfcc4f4bc619bc1877277ccc5cef52b8e017a99
rikurdi <rikukaartoaho@gmail.com>
2021-11-17 08:43:14 +0200

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace OmaNimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Riku Kaartoaho");
+        }
+    }
+}
